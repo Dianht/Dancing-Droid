7a9fb9c37f06983c75f27f348c6bb89edeadab07
DinathSh <dinath22@live.fr>
2020-10-21 03:35:27 +0200

    Le meme programme que le master mais avec des énumération

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-//A FAIRE : crée des fonctions, faire marcher le programme à partir de fichier d'instrucions
+use colored::*;
 
 enum Orientation {
     North,
     South,
-    Est,
     West,
+    Est,
 }
-
-struct Robot<'a>  {
-    id: String,
-    orientation : String,
-    instructions : Vec<&'a str>,
-    x: i16,
-    y: i16,
+enum Instruction {
+    F,
+    L,
+    R,
+}
+struct Robot {
+    id: i32,
+    x: i32,
+    y: i32,
 }
 
 
@@ -20,164 +22,193 @@ struct Robot<'a>  {
 fn main() {
 
     
-    let limite_x : i16 = 3;
-    let limite_y : i16 = 3;
+    let limite_x : i32 = 5;
+    let limite_y : i32 = 5;
     
-    
-    let mut vecteur = vec![Robot{ id: "Blitz".to_string(), orientation: "North".to_string(),instructions: ["F","F","F","F"].to_vec(),x: 1,y: 1 },
-                             Robot{ id: "Dinabot".to_string(), orientation: "North".to_string(),instructions: ["F","F","F"].to_vec(),x: 2,y: 2}];
+    let mut direction_robot1 = Orientation::North;
+    let mut direction_robot2 = Orientation::South;
 
-    let (rb1,rb2) = vecteur.split_at_mut(1);    //Si on fait directement rb1 = &mut robot1[0]et rb = &mut robot2[1]
+    let instruction_robot1 = vec![Instruction::F,Instruction::L,Instruction::L,Instruction::F,Instruction::R,Instruction::F];
+    let instruction_robot2 = vec![Instruction::F,Instruction::F,Instruction::L,Instruction::F,Instruction::R,Instruction::R,Instruction::F];
+    let mut vecteur = vec![Robot{ id: 0,x: 1,y: 1 },
+                             Robot{ id: 1,x: 3,y: 2}];
+
+    let (rb1,rb2) = vecteur.split_at_mut(1);    
     let mut robot1 = &mut rb1[0];
     let mut robot2 = &mut rb2[0];
-
-    let taille = if robot1.instructions.len() > robot2.instructions.len(){              //On prend la plus grande taille des deux vecteurs
-                        robot1.instructions.len()}
-                else {
-                    robot2.instructions.len()
-                };
-
-    println!("Robot<{}> // position initial x : {} y : {}, orientation : {}, instruction{:?}",robot1.id,robot1.x,robot1.y,robot1.orientation,robot1.instructions);
-    println!("Robot<{}> // position initial x : {} y : {}, orientation : {}, instruction{:?}",robot2.id,robot2.x,robot2.y,robot2.orientation,robot2.instructions);
-
-    
+    let taille = 
+    if instruction_robot1.len() > instruction_robot2.len(){              //On prend la plus grande taille des deux vecteurs
+        instruction_robot1.len()
+    }else {
+        instruction_robot2.len()
+    };
 
 
     for i in 0..taille {
-        if i != robot1.instructions.len(){          //Parce que si i est superieur à la taille d'un des deux tableaux, le programme s'arrete du coup on arrete seulement un des dexu match
-            match robot1.instructions[i]{
-                "F" => {
-                    if robot1.orientation == "North"{
-                        if robot1.x == robot2.x && robot1.y + 1 == robot2.y || robot1.y + 1 == limite_y {
-                            println!("Collision ! du Robot<{}> aux coordonnées x:{}, y:{}",robot1.id,robot1.x,robot1.y);
-                        }else {
-                            robot1.y = robot1.y + 1;
+        if i < instruction_robot1.len() {
+            match instruction_robot1[i]{
+                Instruction::F => {
+                    match direction_robot1 {
+                        Orientation::North => {
+                            if robot1.x == robot2.x && robot1.y + 1 == robot2.y {
+                                println!("Collision ! du Robot<{}> aux coordonnées x:{}, y:{}",robot1.id,robot1.x,robot1.y);
+                            }else if robot1.y + 1 == limite_y + 1 {
+                                println!("{}  le Robot<{}>! se dirige vers les limites de la map 😮 !","Attention !".red(),robot1.id);
+                            }else {
+                                robot1.y = robot1.y + 1;
+                            }
                         }
-                    }
-                    else if robot1.orientation == "West"{
-                        if robot1.x - 1 == robot2.x && robot1.y == robot2.y || robot1.x - 1 == limite_x {
-                            println!("Collision ! du Robot<{}> aux coordonnées x:{}, y:{}",robot1.id,robot1.x,robot1.y);
-                        } else {
-                            robot1.x = robot1.x - 1;
+                        Orientation::West => {
+                            if robot2.x - 1 == robot1.x && robot2.y == robot1.y {
+                                println!("Collision ! du Robot<{}> aux coordonnées x:{}, y:{}",robot1.id,robot1.x,robot1.y);
+                            }else if robot2.x - 1 == -1 {
+                                println!("{}  le Robot<{}>! se dirige vers les limites de la map 😮 !","Attention !".red(),robot1.id);
+                            }else {
+                                robot2.x = robot2.x - 1;
+                            }
                         }
-                    }
-                    else if robot1.orientation == "Est"{
-                        if robot1.x + 1 == robot2.x && robot1.y == robot2.y || robot1.x + 1 == limite_x {
-                            println!("Collision ! du Robot<{}> aux coordonnées x:{}, y:{}",robot1.id,robot1.x,robot1.y);
-                        } else {
-                            robot1.x = robot1.x + 1;
+                        Orientation::Est => {
+                            if robot2.x + 1 == robot1.x && robot2.y == robot1.y {
+                                println!("Collision ! du Robot<{}> aux coordonnées x:{}, y:{}",robot1.id,robot1.x,robot1.y);
+                            } else if robot2.x + 1 == limite_x + 1{
+                                println!("{}  le Robot<{}>! se dirige vers les limites de la map 😮 !","Attention !".red(),robot1.id);
+                            }else {    
+                                robot2.x = robot2.x + 1;
+                            }
                         }
-                    }   
-                    else if robot1.orientation == "South"{
-                        if robot1.x == robot2.x && robot1.y - 1 == robot2.y || robot1.y - 1 == limite_y{
-                            println!("Collision ! du Robot<{}> aux coordonnées x:{}, y:{}",robot1.id,robot1.x,robot1.y);
-                        } else {
-                            robot1.y = robot1.y - 1;
+                        Orientation::South =>{
+                            if robot2.x == robot1.x && robot2.y - 1 == robot1.y {
+                                println!("Collision ! du Robot<{}> aux coordonnées x:{}, y:{}",robot1.id,robot1.x,robot1.y);
+                            } else if robot2.y - 1 == -1 {
+                                println!("{}  le Robot<{}>! se dirige vers les limites de la map 😮 !","Attention !".red(),robot1.id);
+                            }else {
+                                robot2.y = robot2.y - 1;
+                            }
                         }
                     }
                 }
-                "L" => {
-                    if robot1.orientation == "North"{
-                        robot1.orientation = "West".to_string();
+                Instruction::L => {
+                    match direction_robot1 {
+                        Orientation::North => {
+                            direction_robot1 = Orientation::West;
+                        }
+                        Orientation::West => {
+                            direction_robot1 = Orientation::South;
+                        }
+                        Orientation::Est => {
+                            direction_robot1 = Orientation::North;
+                        }
+                        Orientation::South => {
+                            direction_robot1 = Orientation::Est;
+                        }
                     }
-                    else if robot1.orientation == "West"{
-                        robot1.orientation = "South".to_string();
-                    }
-                    else if robot1.orientation == "Est"{
-                        robot1.orientation = "North".to_string();
-                    }
-                    else if robot1.orientation == "South"{
-                        robot1.orientation = "Est".to_string();
-                    }
-                
                 }
-                "R" => {
-                    if robot1.orientation == "North"{
-                        robot1.orientation = "Est".to_string();
+                Instruction::R => {
+                    match direction_robot1 {
+                        Orientation::North => {
+                            direction_robot1 = Orientation::Est;
+                        }
+                        Orientation::West => {
+                            direction_robot1 = Orientation::North;
+                        }
+                        Orientation::Est => {
+                            direction_robot1 = Orientation::South;
+                        }
+                        Orientation::South => {
+                            direction_robot1 = Orientation::West;
+                        }
                     }
-                    else if robot1.orientation == "West"{
-                        robot1.orientation = "North".to_string();
-                    }
-                    else if robot1.orientation == "Est"{
-                        robot1.orientation = "South".to_string();
-                    }
-                    else if robot1.orientation == "South"{
-                        robot1.orientation = "West".to_string();
-                    }
-                    }
-                _ => println!("Je reconnais pas votre lettre"),
+                }
             }
         }
-        if i != robot2.instructions.len(){
-            match robot2.instructions[i]{
-                "F" => {
-                    if robot2.orientation == "North"{
-                        if robot2.x == robot1.x && robot2.y + 1 == robot1.y || robot2.y + 1 == limite_y {
-                            println!("Collision ! du Robot <{}> aux  coordonnées x:{}, y:{}",robot2.id,robot2.x,robot2.y);
-                        }else {
-                            robot2.y = robot2.y + 1;
+        if i < instruction_robot2.len() {
+            match instruction_robot2[i]{
+                Instruction::F => {
+                    match direction_robot2 {
+                        Orientation::North => {
+                            if robot2.x == robot1.x && robot2.y + 1 == robot1.y{
+                                println!("Collision ! du Robot <{}> aux  coordonnées x:{}, y:{}",robot2.id,robot2.x,robot2.y);
+                            }else if robot2.y + 1 == limite_y + 1{
+                                println!("{}  le Robot<{}>! se dirige vers les limites de la map 😮 !","Attention !".red(),robot2.id);
+                            }else{
+                                robot2.y = robot2.y + 1;
+                            }
                         }
-                    }
-                    else if robot2.orientation == "West"{
-                        if robot2.x - 1 == robot1.x && robot2.y == robot1.y || robot2.x - 1 == limite_x {
-                            println!("Collision ! du Robot <{}> aux  coordonnées x:{}, y:{}",robot2.id,robot2.x,robot2.y);
-                        } else {
-                            robot2.x = robot2.x - 1;
+                        Orientation::West => {
+                            if robot2.x - 1 == robot1.x && robot2.y == robot1.y{
+                                println!("Collision ! du Robot <{}> aux  coordonnées x:{}, y:{}",robot2.id,robot2.x,robot2.y);
+                            }else if robot2.x - 1 == -1 {
+                                println!("{}  le Robot<{}>! se dirige vers les limites de la map 😮 !","Attention !".red(),robot2.id);
+                            }else {
+                                robot2.x = robot2.x - 1;
+                            }
                         }
-                    }
-                    else if robot2.orientation == "Est"{
-                        if robot2.x + 1 == robot1.x && robot2.y == robot1.y || robot2.x + 1 == limite_x {
-                            println!("Collision ! du Robot <{}> aux  coordonnées x:{}, y:{}",robot2.id,robot2.x,robot2.y);
-                        } else {
-                            robot2.x = robot2.x + 1;
+                        Orientation::Est => {
+                            if robot2.x + 1 == robot1.x && robot2.y == robot1.y{
+                                println!("Collision ! du Robot <{}> aux  coordonnées x:{}, y:{}",robot2.id,robot2.x,robot2.y);
+                            }else if robot2.x + 1 == limite_x + 1{
+                                println!("{}  le Robot<{}>! se dirige vers les limites de la map 😮 !","Attention !".red(),robot2.id);
+                            }else {
+                                robot2.x = robot2.x + 1;
+                            }
                         }
-                    }   
-                    else if robot2.orientation == "South"{
-                        if robot2.x == robot1.x && robot2.y - 1 == robot1.y || robot2.y - 1 == limite_y {
-                            println!("Collision ! du Robot <{}> aux  coordonnées x:{}, y:{}",robot2.id,robot2.x,robot2.y);
-                        } else {
-                            robot2.y = robot2.y - 1;
+                        Orientation::South =>{
+                            if robot2.x == robot1.x && robot2.y - 1 == robot1.y{
+                                println!("Collision ! du Robot <{}> aux  coordonnées x:{}, y:{}",robot2.id,robot2.x,robot2.y);
+                            } else if robot2.y - 1 == - 1{
+                                println!("{}  le Robot<{}>! se dirige vers les limites de la map 😮 !","Attention !".red(),robot2.id);
+                            }else{
+                                robot2.y = robot2.y - 1;
+                            }
                         }
                     }
                 }
-                "L" => {
-                    if robot2.orientation == "North"{
-                        robot2.orientation = "West".to_string();
+                Instruction::L => {
+                    match direction_robot2 {
+                        Orientation::North => {
+                            direction_robot2 = Orientation::West;
+                        }
+                        Orientation::West => {
+                            direction_robot2 = Orientation::South;
+                        }
+                        Orientation::Est => {
+                            direction_robot2 = Orientation::North;
+                        }
+                        Orientation::South => {
+                            direction_robot2 = Orientation::Est;
+                        }
                     }
-                    else if robot2.orientation == "West"{
-                        robot1.orientation = "South".to_string();
-                    }
-                    else if robot2.orientation == "Est"{
-                        robot2.orientation = "North".to_string();
-                    }
-                    else if robot2.orientation == "South"{
-                        robot2.orientation = "Est".to_string();
-                    }
-                
                 }
-                "R" => {
-                    if robot2.orientation == "North"{
-                        robot2.orientation = "Est".to_string();
+                Instruction::R => {
+                    match direction_robot2 {
+                        Orientation::North => {
+                            direction_robot2 = Orientation::Est;
+                        }
+                        Orientation::West => {
+                            direction_robot2 = Orientation::North;
+                        }
+                        Orientation::Est => {
+                            direction_robot2 = Orientation::South;
+                        }
+                        Orientation::South => {
+                            direction_robot2 = Orientation::West;
+                        }
                     }
-                    else if robot2.orientation == "West"{
-                        robot2.orientation = "North".to_string();
-                    }
-                    else if robot2.orientation == "Est"{
-                        robot2.orientation = "South".to_string();
-                    }
-                    else if robot2.orientation == "South"{
-                        robot2.orientation = "West".to_string();
-                    }
-                    }
-                _ => println!("Je reconnais pas votre lettre"),
+                }
             }
         }
         
     }
+    
+    
 
-    println!("position du Robot<{}> final x : {} y : {}",robot1.id,robot1.x,robot1.y);
-    println!("position du Robot<{}> final x : {} y : {}",robot2.id,robot2.x,robot2.y);
 
+
+    
+
+
+    println!("{} du {}<{}> final x : {} y : {}","Position".blue(),"Robot".magenta(),robot1.id,robot1.x,robot1.y);
+    println!("{} du {}<{}> final x : {} y : {}","Position".blue(),"Robot".cyan(),robot2.id,robot2.x,robot2.y);
         
 }
     
