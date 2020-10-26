aac3ba1bbeb11b808abc6cddac88be29d05fef51
DinathSh <dinath22@live.fr>
2020-10-26 14:22:26 +0100

    Possibilité de crée plusieurs robots en cours

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,12 @@ enum Instruction {
     L,
     R,
 }
-struct Robot {
+struct Robot<'a> {
     id: i32,
     x: i32,
     y: i32,
     orientation : Orientation,
+    instruction : Vec<&'a Instruction>,
 }
 
 fn collision(tmp_x : i32,tmp_y :i32,robot1 :&mut Robot,robot2 :&mut Robot,lim_x :i32,lim_y:i32){
@@ -71,31 +72,34 @@ fn main() {
     let instruction_robot1 = vec![f,l,l,f,f,f];
     let instruction_robot2 = vec![r,f,l,f,r,r,f];
     
-    let mut vecteur = vec![Robot{ id: 0,x: 3,y: 3,orientation: Orientation::South},
-                             Robot{ id: 1,x: 3,y: 2,orientation: Orientation::West}];
+    let mut vec = Vec::new();
 
-    let (rb1,rb2) = vecteur.split_at_mut(1);    
-    let  robot1 = &mut rb1[0];
-    let  robot2 = &mut rb2[0];
+    let rb = Robot{ id: 0,x: 1,y: 1,orientation: Orientation::North, instruction : vec![f,l,l,f,f,f]};
+    vec.push(rb);
+    let rb = Robot{ id: 1,x: 3,y: 2,orientation: Orientation::South,instruction : vec![r,f,l,f,r,r,f]};
+    vec.push(rb);
+   
     
     let mut tmp :(i32,i32);
     let taille = if instruction_robot1.len() > instruction_robot2.len(){instruction_robot1.len()}else {instruction_robot2.len()};
 
-
-    for i in 0..taille{
-        if i < instruction_robot1.len() {
-            tmp = (robot1.x,robot1.y);
-            game(instruction_robot1[i],robot1);
-            collision(tmp.0,tmp.1,robot1,robot2,lim_x,lim_y);
-        }
-        if i < instruction_robot2.len() {
-            tmp = (robot2.x,robot2.y);
-            collision(tmp.0,tmp.1,robot2,robot1,lim_x,lim_y);
-            game(instruction_robot2[i],robot2);
+    for x in 0..taille{
+        for i in 0..vec.len(){
+            if x < vec[i].instruction.len() {
+                game(vec[i].instruction[x],& mut vec[i]);
+                
+            }  
         }
     }
-    println!("\n{} du {}<{}>  x : {} y : {}","Position finale".magenta(),"Robot".yellow(),robot1.id,robot1.x,robot1.y);
-    println!("{} du {}<{}> final x : {} y : {}","Position finale".magenta(),"Robot".cyan(),robot2.id,robot2.x,robot2.y);
+    
+    println!("\n{} du {}<{}>  x : {} y : {}","Position finale".magenta(),"Robot".yellow(),vec[0].id,vec[0].x,vec[0].y);
+    println!("{} du {}<{}> final x : {} y : {}","Position finale".magenta(),"Robot".cyan(),vec[1].id,vec[1].x,vec[0].y);
         
 }
-    
+
+
+
+
+
+
+
